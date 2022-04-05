bef05b961d00e84bb74e03d6c9b2f95ef003d0f3
BogdanDevUA <yrabohdan2@gmail.com>
2022-04-05 21:10:43 +0300

    Added support for custom config, for downloading your apps

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #!/usr/bin/env bash
+
+# === COLORS ==============
 reset_fore="\x1b[37m"
 reset_back="\x1b[49m"
 green="\x1b[32m"
@@ -10,6 +12,7 @@ yellow_back="\x1b[43m"
 blue_back="\x1b[44m"
 
 clr=$reset_fore$reset_back
+# =========================
 
 function say() {
   clear
@@ -18,42 +21,61 @@ function say() {
 }
 
 function load() {
+  # Git Clonner
   git clone https://github.com/$1/$2.git
 }
 
 function install() {
   say "${cyan}Installing $2 ..."
   load $1 $2
-  if [[ $3 && $4 && $3=--build ]]; then
-    docker build $2 -t $4
+  if [[ $3 ]]; then
+    # Building
+    docker build $2 -t $3
   fi
   say "${green}$2 installed!"
   sleep "2"
 }
 
+# Ukraine
 echo -e "${blue_back}${yellow}+       +$clr"
 echo -e "${yellow} UA-${blue}DDoS $clr"
 echo -e "${yellow_back}${blue}+       +$clr"
 sleep "3"
 
 say "${green}Installing started"
+# Start
 sleep "1"
 
 mkdir apps
+# Making dir for downloaded apps
 cd apps
 
 sudo pip3 install --upgrade pip > /dev/null
+# Upgrading pip in /dev/nul for without logs
 
-install opengs uashield --build uashield
-install MHProDev MHDDoS --build mhddos
-install palahsu DDoS-Ripper
-install mkdirlove SPAM-EMAIL
+if [[ $1 ]]; then
+  config=$1
+  # Custom config
+else
+  config="basic_config.txt"
+fi
+installed=
+while IFS= read -r line; do 
+  eval "line=$line"
+  len=${#line}
+  if [[ $len ]]; then
+    install ${line[@]}
+    installed="$installed${line[1]} "
+  fi
+done < $config
 
 sleep "2"
 
-say "${green}Installed packages: ${cyan}uashield, MHDDoS, DDoS-Ripper, SPAM-EMAIL"
+say "${green}Installed packages: ${cyan}$installed"
 echo -e "${blue}Glory ${yellow}Ukraine!$reset_fore"
+# Ending
 
 cd ../UA-DDoS
 . functions.sh && cp functions.sh ../../etc/profile.d
+# Integration autorun file for functions
 cd ..
